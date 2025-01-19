eb1730a12e5976e5155e9cb16d8f7ade6fb188c1
adrian <adrianreal234@gmail.com>
2025-01-19 19:42:24 +0100

    Agregar funcionalidad para buscar Pokémon por nombre y mejorar manejo de errores en la obtención de datos

--- src/components/FormularioComponente.jsx
+++ src/components/FormularioComponente.jsx
@@ -1,36 +1,64 @@
-import React from 'react'
-import { useState } from 'react'
-import './FormularioComponente.css'
+import React, { useState } from 'react';
+import './FormularioComponente.css';
 
 function FormularioComponente(props) {
+  const [desde, setDesde] = useState(1);
+  const [hasta, setHasta] = useState(10);
+  const [nombre, setNombre] = useState('');
 
-    const [desde, setDesde] = useState(1);
-    const [hasta, setHasta] = useState(10);
-    const [nombre, setNombre] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        props.getPokemon(desde, hasta);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (nombre) {
+      props.getPokemonByName(nombre);
+    } else {
+      props.getPokemon(desde, hasta);
     }
+  };
+
   return (
     <form onSubmit={handleSubmit}>
-        <h2>Buscar Pokémon</h2>
-        <fieldset className='form-fieldset'>
-            <legend className='legend-fieldset'>Buscar Pokémon por rango</legend>
-            <label htmlFor="desde" className='label-form'>Desde:</label>
-            <input type="number" id="desde" name="desde" value={desde} onChange={(e) => setDesde(e.target.value)} className='input-form' />
-            <label htmlFor="hasta" className='label-form'>Hasta:</label>
-            <input type="number" id="hasta" name="hasta" value={hasta} onChange={(e) => setHasta(e.target.value)} className='input-form' />
-            <button type="submit" className='btn-form'>Buscar</button>
-        </fieldset>
-        <fieldset className='form-fieldset'>
-            <legend className='legend-fieldset'>Buscar Pokémon por nombre</legend>
-            <label htmlFor="desde" className='label-form'>Nombre:</label>
-            <input type="text" id="nombre" name="nombre"  onChange={(e) => setNombre(e.target.value)} className='input-form' />
-            <button type="submit" className='btn-form'>Buscar</button>
-        </fieldset>
+      <h2>Buscar Pokémon</h2>
+      <fieldset className='form-fieldset'>
+        <legend className='legend-fieldset'>Buscar Pokémon por rango</legend>
+        <label htmlFor="desde" className='label-form'>Desde:</label>
+        <input
+          type="number"
+          id="desde"
+          name="desde"
+          value={desde}
+          min="1"
+          max={props.totalPokemon}
+          onChange={(e) => setDesde(e.target.value)}
+          className='input-form'
+        />
+        <label htmlFor="hasta" className='label-form'>Hasta:</label>
+        <input
+          type="number"
+          id="hasta"
+          name="hasta"
+          value={hasta}
+          min="1"
+          max={props.totalPokemon}
+          onChange={(e) => setHasta(e.target.value)}
+          className='input-form'
+        />
+        <button type="submit" className='btn-form'>Buscar</button>
+      </fieldset>
+      <fieldset className='form-fieldset'>
+        <legend className='legend-fieldset'>Buscar Pokémon por nombre</legend>
+        <label htmlFor="nombre" className='label-form'>Nombre:</label>
+        <input
+          type="text"
+          id="nombre"
+          name="nombre"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+          className='input-form'
+        />
+        <button type="submit" className='btn-form'>Buscar</button>
+      </fieldset>
     </form>
-  )
+  );
 }
 
-export default FormularioComponente
+export default FormularioComponente;
--- src/components/PokemonListComponent.jsx
+++ src/components/PokemonListComponent.jsx
@@ -12,19 +12,54 @@ function PokemonListComponent(props) {
     getPokemonList(1, 10);
   }, []);
 
-  const fetchPokemons = async (index) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`);
-    const data = await response.json();
-    return data;
+  
+
+  const fetchPokemon = async (index) => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      
+      return null;
+    }
+  };
+
+  const fetchPokemonByName = async (name) => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(`Error fetching Pokémon by name ${name}:`, error);
+      return null;
+    }
   };
 
   const getPokemonList = async (desde, hasta) => {
     const pokemonArray = [];
     for (let i = desde; i <= hasta; i++) {
-      const pokemon = await fetchPokemons(i);
-      pokemonArray.push(pokemon);
+      const pokemon = await fetchPokemon(i);
+      if (pokemon) {
+        pokemonArray.push(pokemon);
+      } else {
+        break;
+      }
     }
     setPokemonList(pokemonArray);
+  };
+
+  const getPokemonByName = async (name) => {
+      const pokemon = await fetchPokemonByName(name);
+      if (pokemon) {
+        setPokemonList([pokemon]);
+      } else {
+        setPokemonList([]);
+      }
+    
   };
 
   const pokemonCards = pokemonList.map((pokemon) => (
@@ -37,7 +72,7 @@ function PokemonListComponent(props) {
 
   return (
     <div>
-      <FormularioComponente getPokemon={getPokemonList} />
+      <FormularioComponente getPokemon={getPokemonList} getPokemonByName={getPokemonByName}/>
       {selectedPokemon && (
         <div className='selected-pokemon'>
           <h2>Pokémon seleccionado: {selectedPokemon.name}</h2>
